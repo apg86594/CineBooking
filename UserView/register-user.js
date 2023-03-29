ebf4454c521399d462c28178fd06200716e88da1
rdpett99 <rdp06865@uga.edu>
2023-03-29 16:48:39 -0400

    updated script for register.html

--- UserView/register-user.js
+++ UserView/register-user.js
@@ -1,64 +1,112 @@
+/* Script for register.html */
+
+var socket = null;
+
 /*
- * Registers the user with the server.
+ * Connects to the server when the page is initially loaded and
+ * adds an event listener to the Sign Up button.
  */
-const WebSocket = require("ws");
-const fs        = require("fs");
-const {JSDOM}   = require("jsdom");
-
-const html      = fs.readFileSync("UserView/register.html");
-const dom       = new JSDOM(html);
-const document  = dom.window.document;
-
-function register_user()
+function initialize()
 {
-    const socket = new WebSocket("ws://127.0.0.1:8888");
-    console.log("YOU MADE IT HERE");
-    // Required fields for registering a user
-    const password      = document.getElementById("psw");
-    const firstname     = document.getElementById("firstname");
-    const lastname      = document.getElementById("lastname");
-    const email         = document.getElementById("email");
-    const USERTYPE      = 1;
-    const billingZIP    = document.getElementById("billingZIP");
-    const cardnum       = document.getElementById("cardnum");
-    const cvv           = document.getElementById("cvv");
-    const month         = document.getElementById("month");
-    const year          = document.getElementById("year");
-
-    socket.onopen = function(event) {
-        console.log("Connected to server");
-        socket.send(`REGISTER,
-                ${password}, 
-                ${firstname}, 
-                ${lastname}, 
-                ${email}, 
-                ${USERTYPE}, 
-                ${billingZIP},
-                ${cardnum},
-                ${cvv},
-                ${month},
-                ${year}`);
-    };
-
-    socket.onmessage = function(event) {
-        console.log(`Received message.\n[MESSAGE]: ${event.data}\nClosing connection.`);
-        socket.close();
+    socket = new WebSocket("ws://127.0.0.1:8888");
+    socket.onopen = () => {
+        console.log("Connection to server established.");
+        var button = document.getElementById("signupbtn");
+        button.addEventListener("click", registerUser);
     }
-    
-    socket.onclose = function(event) {
-        if (event.wasClean) {
-          console.log(`[CLOSE] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-        } else {
-          // e.g. server process killed or network down
-          // event.code is usually 1006 in this case
-          console.log('[CLOSE] Connection died');
-        }
-    };
-      
-    socket.onerror = function(error) {
-        console.log(`[ERROR]: ${error}`);
-    };
 }
 
-const btn = document.getElementById("signupbtn");
-if (btn) { btn.addEventListener("click", register_user); }
+/*
+ * Sends the information submitted by the user to the server through
+ * the WebSocket.
+ */
+function registerUser()
+{
+    const psw           = document.getElementById("psw").value;
+    const first         = document.getElementById("firstName").value;
+    const last          = document.getElementById("lastName").value;
+    const email         = document.getElementById("Email").value;
+    const USERTYPE      = 1;
+    const billingZIP    = document.getElementById("billingZIP").value;
+    const cardnum       = document.getElementById("cardnum").value;
+    const cvv           = document.getElementById("cvv").value;
+    const month         = document.getElementById("Month").value;
+    const year          = document.getElementById("Year").value;
+
+    const _month = convertMonthToInt(month);
+    const _year  = year.slice(-2);
+
+    const message = `REGISTER,${psw},${first},${last},${email},${USERTYPE},${billingZIP},${cardnum},${cvv},${_month},${_year}`;
+        
+    socket.send(message);
+        
+    socket.addEventListener("message", (event) => {
+        console.log("Message received from server:", event.data);
+        if (event.data === "SUCCESS") { 
+
+            /*** NEED TO REDIRECT TO register_suc.html HERE */
+
+            //window.location.href = "CineBooking/UserView/register_suc.html";
+
+            socket.close();
+        }
+    });
+        
+    socket.addEventListener("close", (event) => {
+        console.log("WebSocket connection closed with code:", event.code);
+    });
+        
+    socket.addEventListener("error", (event) => {
+        console.error("WebSocket connection error:", event);
+    });
+}
+
+/*
+ * Converts str_month to its numerical value.
+ */
+function convertMonthToInt(str_month)
+{
+    var m = "";
+    switch (str_month)
+    {
+        case "january":
+            m = "01";
+            break;
+        case "february":
+            m = "02";
+            break;
+        case "march":
+            m = "03";
+            break;
+        case "april":
+            m = "04";
+            break;
+        case "may":
+            m = "05";
+            break;
+        case "june":
+            m = "06";
+            break;
+        case "july":
+            m = "07";
+            break;
+        case "august":
+            m = "08";
+            break;
+        case "september":
+            m = "09";
+            break;
+        case "october":
+            m = "10";
+            break; 
+        case "november":
+            m = "11";
+            break;
+        case "december":
+            m = "12";
+            break;   
+    }
+    return m;
+}
+
+window.onload = initialize;
