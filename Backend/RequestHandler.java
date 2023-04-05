232bb2241d5793b4114c8a6d10210324dc67fddb
rdpett99 <rdp06865@uga.edu>
2023-04-05 15:36:09 -0400

    changed editUser

--- Backend/RequestHandler.java
+++ Backend/RequestHandler.java
@@ -12,7 +12,7 @@ public class RequestHandler {
     private ResultSet results;
     String url = "jdbc:MySQL://localhost:3306/cinemabookingsystem"; // change port if server is on different port
     String username = "root"; // set user name to local server username
-    String password = "Test123"; // set password to local server password
+    String password = "password123"; // set password to local server password
     final String secretKey = "ylwqc";
     SendEmail email = new SendEmail();
 
@@ -201,7 +201,7 @@ public class RequestHandler {
             results = findUserStmt.executeQuery();
             if(results.next()) { 
                 String userEmail = results.getString("email");
-                String sql = "UPDATE user SET password = ?, firstName=?, lastName=?, email=?, USERTYPE=?, billingAddress=?, ACTIVE=?, cardnum=?, securitynum=?, expmonth=?,expdate=?" +
+                String sql = "UPDATE user SET password = ?, firstName=?, lastName=?, email=?, USERTYPE=?, billingAddress=?, cardnum=?, securitynum=?, expmonth=?,expdate=?" +
                     "WHERE email = ?";
                     PreparedStatement preparedStmt = connection.prepareStatement(sql);
             try {
@@ -211,12 +211,11 @@ public class RequestHandler {
                 preparedStmt.setString(4, inputs[4]); //email
                 preparedStmt.setString(5, inputs[5]); //usertype
                 preparedStmt.setString(6, inputs[6]); //billingaddress
-                preparedStmt.setString(7, "1"); //active
-                preparedStmt.setString(8, encrypter.encrypt(inputs[7],secretKey)); // cardnum
-                preparedStmt.setString(9, encrypter.encrypt(inputs[8],secretKey)); // securitynum
-                preparedStmt.setString(10,inputs[9]); // expmonth
-                preparedStmt.setString(11,inputs[10]); // expdate
-                preparedStmt.setString(12, userEmail);
+                preparedStmt.setString(7, encrypter.encrypt(inputs[7],secretKey)); // cardnum
+                preparedStmt.setString(8, encrypter.encrypt(inputs[8],secretKey)); // securitynum
+                preparedStmt.setString(9,inputs[9]); // expmonth
+                preparedStmt.setString(10,inputs[10]); // expdate
+                preparedStmt.setString(11, userEmail);
                 preparedStmt.execute();
             } catch (SQLException e) {
                 // TODO Auto-generated catch block
