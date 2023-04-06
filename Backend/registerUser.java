0dbad95cb980cbce132524dc7afa30d5740e6d80
Charlie Skinner <ces46494@uga.edu>
2023-04-05 21:12:24 -0400

    Removed deprecated import statements

--- Backend/registerUser.java
+++ Backend/registerUser.java
@@ -1,12 +1,10 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Random;
 import java.io.*;
-import org.json.simple.JSONObject;
+
 
 public class registerUser {
 
